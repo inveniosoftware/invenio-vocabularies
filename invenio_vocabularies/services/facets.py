b3e49562a2421cea5fa7d7261aead8f761d54986
Pablo Panero <pablo.panero@cern.ch>
2021-05-19 10:02:38 +0200

    services: add facet labelling

---
+++ invenio_vocabularies/services/facets.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Vocabulary facets."""
+
+from flask_principal import AnonymousIdentity
+from invenio_i18n.ext import current_i18n
+from marshmallow_utils.fields.babel import gettext_from_dict
+from speaklater import make_lazy_string
+
+from ..proxies import current_service
+
+
+def lazy_get_label(vocab_item):
+    """Lazy evaluation of a localized vocabulary label."""
+    params = {
+        "locale": current_i18n.locale,
+        "default_locale": "en"
+    }
+
+    return make_lazy_string(
+        gettext_from_dict, vocab_item, **params)
+
+
+class VocabularyLabels:
+    """Fetching of vocabulary labels for facets."""
+
+    def __init__(self, vocabulary, cache=False):
+        """Initialize the labels."""
+        self.vocabulary = vocabulary
+        self.cache = cache
+        self.fields = ["id", "title"]  # not configurable
+
+    def __call__(self, ids):
+        """Return the mapping when evaluated."""
+        identity = AnonymousIdentity()
+
+        if not self.cache:
+            vocabs = current_service.read_many(
+                identity, ids, fields=self.fields)
+        else:
+            vocabs = current_service.read_all(identity, fields=self.fields)
+
+        labels = {}
+        vocab_list = list(vocabs.hits)  # the service returns a generator
+        ids = set(ids)
+        seen = set()
+        for vocab in vocab_list:
+            # cannot loop over ids because vocab id is inside each item
+            if len(ids) == len(seen):
+                break
+
+            id_ = vocab["id"]
+            if id_ in ids:
+                labels[id_] = lazy_get_label(vocab["title"])
+                seen.add(id_)
+
+        return labels
---
+++ tests/services/test_facets.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Test the vocabulary service facets."""
+
+import pytest
+
+from invenio_vocabularies.records.api import Vocabulary
+from invenio_vocabularies.services.facets import VocabularyLabels
+
+
+@pytest.fixture(scope='module')
+def example_data():
+    """Example data."""
+    return [
+        {
+            'id': 'eng',
+            'title': {'en': 'English', 'da': 'Engelsk'},
+            'type': 'languages',
+        },
+        {
+            'id': 'spa',
+            'title': {'en': 'Spanish', 'da': 'Spansk'},
+            'type': 'languages',
+        },
+        {
+            'id': 'fra',
+            'title': {'en': 'French', 'da': 'Fransk'},
+            'type': 'languages',
+        },
+    ]
+
+
+@pytest.fixture(scope='module')
+def example_records(database, identity, service, example_data):
+    """Create some example records."""
+    service.create_type(identity, 'languages', 'lng')
+    records = []
+    for data in example_data:
+        records.append(service.create(identity, data))
+
+    Vocabulary.index.refresh()
+    return records
+
+
+def _assert_vocabs(labels_instance):
+    labels = labels_instance(["eng", "spa"])
+
+    assert labels["eng"] == "English"
+    assert labels["spa"] == "Spanish"
+
+
+def test_vocabulary_label_cache(example_records):
+    # Uses example record to have two languages created on the vocabulary
+    labels_instance = VocabularyLabels(vocabulary="languages", cache=True)
+    _assert_vocabs(labels_instance)
+
+
+def test_vocabulary_label_no_cache(example_records):
+    # Uses example record to have two languages created on the vocabulary
+    labels_instance = VocabularyLabels(vocabulary="languages", cache=False)
+    _assert_vocabs(labels_instance)
+
+
+def test_vocabulary_label_not_found(example_records):
+    # Uses example record to have two languages created on the vocabulary
+    labels_instance = VocabularyLabels(vocabulary="languages", cache=True)
+    labels = labels_instance(["eng", "glg"])
+
+    assert labels["eng"] == "English"
+    assert not labels.get("glg")  # search wont fail
