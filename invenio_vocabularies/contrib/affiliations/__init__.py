ec5303b554052e2126caae7c0c4aa420ee66726c
Guillaume Viger <fenekku@fenekku.com>
2021-07-14 14:34:51 -0400

    affiliations: expose resource/service via __init__

--- invenio_vocabularies/contrib/affiliations/__init__.py
+++ invenio_vocabularies/contrib/affiliations/__init__.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
 #
 # Invenio-Vocabularies is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
 """Affiliations module."""
+
+from .resources import AffiliationsResource, AffiliationsResourceConfig
+from .services import AffiliationsService, AffiliationsServiceConfig
+
+__all__ = [
+    "AffiliationsResource",
+    "AffiliationsResourceConfig",
+    "AffiliationsService",
+    "AffiliationsServiceConfig",
+]
