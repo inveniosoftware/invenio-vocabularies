18c552241a13e1d6827cafe88fbfb3f0937d7a96
Pablo Panero <pablo.panero@cern.ch>
2021-10-18 14:31:56 +0200

    names: add custom pid provider and identifier level resolution

--- invenio_vocabularies/contrib/names/resources.py
+++ invenio_vocabularies/contrib/names/resources.py
@@ -9,8 +9,51 @@
 
 """Names vocabulary resources."""
 
+from flask import g
+from flask_resources import resource_requestctx, response_handler, route
+from invenio_records_resources.resources.records.resource import \
+    request_view_args
+from marshmallow import fields
+
 from .names import record_type
 
-NamesResourceConfig = record_type.resource_config_cls
 
-NamesResource = record_type.resource_cls
+class NamesResourceConfig(record_type.resource_config_cls):
+    """Name resource."""
+
+    routes = record_type.resource_config_cls.routes
+    routes["item-names-resolve"] = "/<pid_type>/<pid_value>"
+    request_view_args = {
+        "pid_value": fields.Str(),
+        "pid_type": fields.Str(),
+    }
+
+
+class NamesResource(record_type.resource_cls):
+    """Name resource."""
+
+    def create_url_rules(self):
+        """Create the URL rules for the record resource."""
+        routes = self.config.routes
+        url_rules = super(NamesResource, self).create_url_rules()
+        url_rules += [
+            route(
+                "GET",
+                routes["item-names-resolve"],
+                self.name_resolve_by_id
+            ),
+        ]
+
+        return url_rules
+
+    @request_view_args
+    @response_handler()
+    def name_resolve_by_id(self):
+        """Resolve an identifier."""
+        item = self.service.resolve(
+            id_=resource_requestctx.view_args["pid_value"],
+            id_type=resource_requestctx.view_args["pid_type"],
+            identity=g.identity,
+        )
+
+        return item.to_dict(), 200
