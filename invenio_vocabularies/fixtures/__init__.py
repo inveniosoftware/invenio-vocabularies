3565234233204b36725cd6295d7d6a3db91439ca
Pablo Panero <pablo.panero@cern.ch>
2021-11-03 14:28:08 +0100

    fixtures: class design

---
+++ invenio_vocabularies/fixtures/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Fixtures module."""
+
+from .fixtures import BaseFixture
+
+__all__ = (
+    "BaseFixture",
+)
---
+++ tests/fixtures/conftest.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Pytest configuration.
+
+See https://pytest-invenio.readthedocs.io/ for documentation on which test
+fixtures are available.
+"""
+
+import pytest
+
+from invenio_vocabularies.fixtures.errors import TransformerError, WriterError
+from invenio_vocabularies.fixtures.readers import BaseReader
+from invenio_vocabularies.fixtures.transformers import BaseTransformer
+from invenio_vocabularies.fixtures.writers import BaseWriter
+
+
+class TestReader(BaseReader):
+    """Test reader."""
+
+    def read(self, *args, **kwargs):
+        """Yields the values in the origin."""
+        for value in self._origin:
+            yield value
+
+
+class TestTransformer(BaseTransformer):
+    """Test transformer."""
+
+    def apply(self, entry, *args, **kwargs):
+        """Sums up one to the value."""
+        if entry < 0:
+            raise TransformerError("Value cannot be negative")
+
+        return entry + 1
+
+
+class TestWriter(BaseWriter):
+    """Test reader."""
+
+
+class FailingTestWriter(BaseWriter):
+    """Test reader."""
+
+    def __init__(self, fail_on):
+        """Initialise error."""
+        super().__init__()
+        self.fail_on = fail_on
+
+    def write(self, entry):
+        """Return the entry."""
+        if entry == self.fail_on:
+            raise WriterError(f"{self.fail_on} value found.")
+
+
+@pytest.fixture(scope='module')
+def app_config(app_config):
+    """Mimic an instance's configuration."""
+    app_config["VOCABULARIES_DATASOURCE_READERS"] = {
+        "test": TestReader
+    }
+    app_config["VOCABULARIES_DATASOURCE_TRANSFORMERS"] = {
+        "test": TestTransformer
+    }
+    app_config["VOCABULARIES_DATASOURCE_WRITERS"] = {
+        "test": TestWriter,
+        "fail": FailingTestWriter,
+    }
+
+    return app_config
