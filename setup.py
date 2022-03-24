6129c702074f85384b6c3f8bc26c2438eb0ad870
Pablo Panero <pablo.panero@cern.ch>
2022-03-24 09:53:07 +0100

    dependencies: bump invenio-records-resources

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup_requires = [
 ]
 
 install_requires = [
-    "invenio-records-resources>=0.19.3,<0.20.0",
+    "invenio-records-resources>=0.19.4,<0.20.0",
     "invenio-i18n>=1.3.1",
     "lxml>=4.5.0",
     "PyYAML>=5.4.1",
