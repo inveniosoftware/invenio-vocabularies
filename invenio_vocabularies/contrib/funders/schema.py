25ba54bbaee5fd89794067b2c34c6bea20e7fc16
Pablo Panero <pablo.panero@cern.ch>
2022-07-13 15:31:38 +0200

    global: add support for custom fields and feature branches tests
    Co-authored-by: Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>

--- invenio_vocabularies/contrib/funders/schema.py
+++ invenio_vocabularies/contrib/funders/schema.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021 CERN.
+# Copyright (C) 2021-2022 CERN.
 #
 # Invenio-Vocabularies is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,7 +12,6 @@ from functools import partial
 
 from flask_babelex import lazy_gettext as _
 from marshmallow import (
-    Schema,
     ValidationError,
     fields,
     post_load,
@@ -23,32 +22,18 @@ from marshmallow import (
 from marshmallow_utils.fields import IdentifierSet, SanitizedUnicode
 from marshmallow_utils.schemas import IdentifierSchema
 
-from ...services.schema import BaseVocabularySchema
+from ...services.schema import BaseVocabularySchema, ContribVocabularyRelationSchema
 from .config import funder_schemes
 
 
-class FunderRelationSchema(Schema):
+class FunderRelationSchema(ContribVocabularyRelationSchema):
     """Funder schema."""
 
+    ftf_name = "name"
+    parent_field_name = "funder"
     name = SanitizedUnicode(
         validate=validate.Length(min=1, error=_("Name cannot be blank."))
     )
-    id = SanitizedUnicode()
-
-    @validates_schema
-    def validate_funder(self, data, **kwargs):
-        """Validates that either id either name are present."""
-        id_ = data.get("id")
-        name = data.get("name")
-        if id_:
-            data = {"id": id_}
-        elif name:
-            data = {"name": name}
-
-        if not id_ and not name:
-            raise ValidationError(
-                _("An existing id or a free text name must be present."), "funder"
-            )
 
 
 class FunderSchema(BaseVocabularySchema):
