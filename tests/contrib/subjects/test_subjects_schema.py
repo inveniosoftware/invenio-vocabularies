94e41e12a9e820203f8299d2b2dcc7c151943c2e
Guillaume Viger <fenekku@fenekku.com>
2022-09-19 14:17:27 -0400

    subject schema: dump scheme
    closes #192

--- tests/contrib/subjects/test_subjects_schema.py
+++ tests/contrib/subjects/test_subjects_schema.py
@@ -7,7 +7,8 @@
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
-"""Affiliations Marshmallow schema tests."""
+"""Subjects schema tests."""
+
 from copy import copy
 
 import pytest
@@ -19,47 +20,50 @@ from invenio_vocabularies.contrib.subjects.schema import (
 )
 
 
-def test_valid_full(subject_full_data):
-    loaded = SubjectSchema().load(subject_full_data)
-    assert subject_full_data == loaded
+class TestSubjectSchema:
+    def test_valid_full(self, subject_full_data):
+        loaded = SubjectSchema().load(subject_full_data)
+        assert subject_full_data == loaded
+
+    def test_invalid_missing_field(self, subject_full_data):
+        # no id
+        invalid = copy(subject_full_data)
+        del invalid["id"]
+        with pytest.raises(ValidationError):
+            SubjectSchema().load(invalid)
+
+        # no scheme
+        invalid = copy(subject_full_data)
+        del invalid["scheme"]
+        with pytest.raises(ValidationError):
+            SubjectSchema().load(invalid)
+
+        # no subject
+        invalid = copy(subject_full_data)
+        del invalid["subject"]
+        with pytest.raises(ValidationError):
+            SubjectSchema().load(invalid)
 
 
-def test_invalid_missing_field(subject_full_data):
-    # no id
-    invalid = copy(subject_full_data)
-    del invalid["id"]
-    with pytest.raises(ValidationError):
-        SubjectSchema().load(invalid)
+class TestSubjectRelationSchema:
+    def test_valid_id(self):
+        valid_id = {
+            "id": "test",
+        }
+        assert valid_id == SubjectRelationSchema().load(valid_id)
 
-    # no scheme
-    invalid = copy(subject_full_data)
-    del invalid["scheme"]
-    with pytest.raises(ValidationError):
-        SubjectSchema().load(invalid)
+    def test_valid_subject(self):
+        valid_subject = {"subject": "Entity One"}
+        assert valid_subject == SubjectRelationSchema().load(valid_subject)
 
-    # no subject
-    invalid = copy(subject_full_data)
-    del invalid["subject"]
-    with pytest.raises(ValidationError):
-        SubjectSchema().load(invalid)
+    def test_invalid_empty(self):
+        invalid_empty = {}
+        with pytest.raises(ValidationError):
+            SubjectRelationSchema().load(invalid_empty)
 
+    def test_dump(self):
+        subject = {"subject": "Entity One"}
+        assert {"subject": "Entity One"} == SubjectRelationSchema().dump(subject)
 
-#
-# SubjectRelationSchema
-#
-def test_valid_id():
-    valid_id = {
-        "id": "test",
-    }
-    assert valid_id == SubjectRelationSchema().load(valid_id)
-
-
-def test_valid_subject():
-    valid_subject = {"subject": "Entity One"}
-    assert valid_subject == SubjectRelationSchema().load(valid_subject)
-
-
-def test_invalid_empty():
-    invalid_empty = {}
-    with pytest.raises(ValidationError):
-        data = SubjectRelationSchema().load(invalid_empty)
+        subject = {"subject": "Entity One", "scheme": "scheme A"}
+        assert subject == SubjectRelationSchema().dump(subject)
