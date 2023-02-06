28f8aa6552d5446e9756e9a89594c1ed7bab6bb6
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-02-06 23:21:08 +0100

    tests: fix extract_messages --dry-run

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2020 CERN.
 # Copyright (C) 2021 Northwestern University.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # Invenio-Vocabularies is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -22,7 +22,7 @@ function cleanup {
 trap cleanup EXIT
 
 python -m check_manifest
-python -m setup extract_messages --dry-run
+python -m setup extract_messages --output-file /dev/null
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${ES:-opensearch} --mq ${CACHE:-redis} --env)"
 python -m pytest $@
