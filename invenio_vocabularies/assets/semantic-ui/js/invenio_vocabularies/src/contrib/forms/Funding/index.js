7fc384812ab23350812483bdfb205db32fcb8311
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-04-17 11:01:15 +0200

    assets: add UI contrib components

---
+++ invenio_vocabularies/assets/semantic-ui/js/invenio_vocabularies/src/contrib/forms/Funding/index.js
@@ -0,0 +1,8 @@
+// This file is part of InvenioVocabularies
+// Copyright (C) 2021-2023 CERN.
+// Copyright (C) 2021 Northwestern University.
+//
+// Invenio is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+export { FundingField } from "./FundingField";
---
+++ invenio_vocabularies/assets/semantic-ui/js/invenio_vocabularies/src/index.js
@@ -0,0 +1,7 @@
+// This file is part of InvenioVocabularies
+// Copyright (C) 2023 CERN.
+//
+// Invenio is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+export * from "./contrib";
