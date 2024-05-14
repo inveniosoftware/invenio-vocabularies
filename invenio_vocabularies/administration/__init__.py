4d3365b979bc9092b586475a6925922d52880c6d
Karl-Ulrich Krägelin <k.kraegelin@uni-muenster.de>
2024-05-14 11:08:31 +0200

    add copyright

--- invenio_vocabularies/administration/__init__.py
+++ invenio_vocabularies/administration/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 ULB Münster.
+# Copyright (C) 2020-2021 CERN.
+# Copyright (C) 2024 Uni Münster.
 #
-# invenio-oaiharvest-config is free software; you can redistribute it and/or
+# Invenio-Vocabularies is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
