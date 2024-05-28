e2065d62fcc755416ccb10c14c8f7a0666655792
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2024-05-28 10:18:32 +0200

    vocabulary types: Show count in list view

--- invenio_vocabularies/services/service.py
+++ invenio_vocabularies/services/service.py
@@ -62,13 +62,13 @@ class VocabularyMetadataList(ServiceListResult):
     """Ensures that vocabulary metadata is returned in the proper format."""
 
     def __init__(
-            self,
-            service,
-            identity,
-            results,
-            params=None,
-            links_tpl=None,
-            links_item_tpl=None,
+        self,
+        service,
+        identity,
+        results,
+        params=None,
+        links_tpl=None,
+        links_item_tpl=None,
     ):
         """Constructor.
 
@@ -181,7 +181,7 @@ class VocabularyTypeService(RecordService):
                 "pid_type": db_vocab_type.pid_type,
                 "count": count_terms_agg.get(db_vocab_type.id, 0),
                 "is_custom_vocabulary": db_vocab_type.id
-                                        in self.custom_vocabulary_names,
+                in self.custom_vocabulary_names,
             }
 
             if db_vocab_type.id in config_vocab_types:
@@ -231,8 +231,8 @@ class VocabularySearchOptions(SearchOptions):
     """Search options."""
 
     params_interpreters_cls = [
-                                  FilterParam.factory(param="tags", field="tags"),
-                              ] + SearchOptions.params_interpreters_cls
+        FilterParam.factory(param="tags", field="tags"),
+    ] + SearchOptions.params_interpreters_cls
 
     suggest_parser_cls = SuggestQueryParser.factory(
         fields=[
@@ -282,7 +282,7 @@ class VocabularySearchOptions(SearchOptions):
         "id": dict(
             title=_("ID"),
             fields=["id"],
-        )
+        ),
     }
 
     pagination_options = {
@@ -355,7 +355,7 @@ class VocabulariesService(RecordService):
         return type_
 
     def search(
-            self, identity, params=None, search_preference=None, type=None, **kwargs
+        self, identity, params=None, search_preference=None, type=None, **kwargs
     ):
         """Search for vocabulary entries."""
         self.require_permission(identity, "search")
