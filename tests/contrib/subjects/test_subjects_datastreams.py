029441c8af57026b013d9c79e7cd8e25b9a52558
Karl-Ulrich Krägelin <k.kraegelin@uni-muenster.de>
2024-05-28 14:13:04 +0200

    remove test

--- tests/contrib/subjects/test_subjects_datastreams.py
+++ tests/contrib/subjects/test_subjects_datastreams.py
@@ -9,37 +9,6 @@ from invenio_vocabularies.contrib.subjects.datastreams import (
 from invenio_vocabularies.datastreams import StreamEntry
 
 
-def test_read():
-    # Arrange
-    mock_yaml_data = [
-        [
-            {
-                "id": "http://www.oecd.org/science/inno/38235147.pdf?6.5",
-                "scheme": "FOS",
-                "subject": "Other humanities",
-            }
-        ]
-    ]
-    reader = SubjectsYAMLReader(options={"filename": "subjects.yaml"})
-
-    with patch(
-        "builtins.open",
-        mock_open(
-            read_data='- id: "http://www.oecd.org/science/inno/38235147.pdf?6.5"\\n  scheme: FOS\\n  subject: "Other humanities"'
-        ),
-    ) as m:
-        with patch("yaml.safe_load", return_value=mock_yaml_data) as mock_yaml:
-
-            data = list(reader.read())  # Convert the generator to a list to consume it
-
-            m.assert_called_once_with("subjects.yaml", "r")
-            mock_yaml.assert_called_once()
-
-            assert (
-                data == mock_yaml_data
-            )  # Check that the data returned by read is correct
-
-
 @pytest.fixture(scope="module")
 def dict_subject_entry():
     return StreamEntry(
