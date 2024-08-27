23e63979d9251a36dc4c7b5ef3fe974c29527f39
Pablo Tamarit <pablo.tamarit@cern.ch>
2024-08-27 17:03:20 +0200

    awards: added CORDIS datastreams

--- invenio_vocabularies/datastreams/transformers.py
+++ invenio_vocabularies/datastreams/transformers.py
@@ -32,6 +32,13 @@ class BaseTransformer(ABC):
 class XMLTransformer(BaseTransformer):
     """XML transformer."""
 
+    def __init__(
+        self, root_element=None, *args, **kwargs
+    ):
+        """Initializes the transformer."""
+        self.root_element = root_element
+        super().__init__(*args, **kwargs)
+
     @classmethod
     def _xml_to_etree(cls, xml):
         """Converts XML to a lxml etree."""
@@ -43,10 +50,14 @@ class XMLTransformer(BaseTransformer):
         Requires the root element to be named "record".
         """
         xml_tree = self._xml_to_etree(stream_entry.entry)
-        record = etree_to_dict(xml_tree)["html"]["body"].get("record")
+        xml_dict = etree_to_dict(xml_tree)["html"]["body"]
 
-        if not record:
-            raise TransformerError(f"Record not found in XML entry.")
+        if self.root_element:
+            record = xml_dict.get(self.root_element)
+            if not record:
+                raise TransformerError(f"Root element '{self.root_element}' not found in XML entry.")
+        else:
+            record = xml_dict
 
         stream_entry.entry = record
         return stream_entry
