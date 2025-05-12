4ad71aed13051713003079ef41588f385856f423
Sam Arbid <samaa2@kth.se>
2025-05-12 12:52:50 +0200

    i18n: refactor compile catalog script
    * add JavaScript translation instructions
    
    * fix: update .gitignore to include node_modules
    and remove unnecessary entries from package.json
    
    * fix: update Transifex configuration

--- invenio_vocabularies/assets/semantic-ui/translations/invenio_vocabularies/messages/index.js
+++ invenio_vocabularies/assets/semantic-ui/translations/invenio_vocabularies/messages/index.js
@@ -1,1 +1,19 @@
-export const translations = {};
+// Copyright (C) 2024 Graz University of Technology.
+
+/*
+This will import translations from an auto-generated file in order to not have to manually adapt the provided languages.
+The auto generated file will be created when running npm run compile_catalog
+File structure of the generated file will look like:
+import TRANSLATE_de from "./de/translations.json";
+import TRANSLATE_en from "./en/translations.json";
+import TRANSLATE_es from "./es/translations.json";
+export const translations = {
+  de: { translation: TRANSLATE_de },
+  en: { translation: TRANSLATE_en },
+  es: { translation: TRANSLATE_es },
+};
+*/
+
+import { translations } from "./_generatedTranslations";
+
+export { translations };
