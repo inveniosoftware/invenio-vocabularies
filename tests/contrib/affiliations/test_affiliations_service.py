99a50c2ae052a1cb839bc6b50b60709814d2e461
Pal Kerecsenyi <pal@p.kerecs.com>
2025-07-08 13:19:36 +0200

    fix: remove `arrow` from tests
    * Removed `arrow` package from tests, so `invenio-vocabularies` no
    longer has any usage of `arrow`
    
    * Updated ROR tests to use full ISO timestamps which are compatible with
    the refactored job implementation

--- tests/contrib/affiliations/test_affiliations_service.py
+++ tests/contrib/affiliations/test_affiliations_service.py
@@ -9,7 +9,7 @@
 
 """Test the affiliation vocabulary service."""
 
-import arrow
+from datetime import datetime, timezone
 import pytest
 from invenio_pidstore.errors import PIDAlreadyExists
 from marshmallow.exceptions import ValidationError
@@ -82,9 +82,9 @@ def test_extra_fields(app, db, service, identity, affiliation_full_data):
 
 
 def test_indexed_at_query(app, db, service, identity, affiliation_full_data):
-    before = arrow.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    before = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")
     _ = service.create(identity, affiliation_full_data)
-    now = arrow.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    now = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")
     Affiliation.index.refresh()
 
     # there is previous to before
